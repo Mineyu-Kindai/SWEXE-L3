18b0c8cddee06e6fd86702a366c14f887744b739
Mineyu <2311140072d@ed.fuk.kindai.ac.jp>
2025-10-01 08:29:00 +0000

    add template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  def aaa
+    logger.debug "-----Debug Debug Debug Debug Debug Debug-----"
+  end
+  
   allow_browser versions: :modern
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    aaa
   end
   
   def new
